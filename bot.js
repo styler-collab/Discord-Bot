d8c968b368a7afea75b5fcc6e355aaf40ae0d3d8
styler-collab <59663629+styler-collab@users.noreply.github.com>
2020-02-06 00:01:05 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
+//functionally this code just does !join and !leave voice chats and stores everything said locally
 //nodemon + ffmpeg + node-opus add to machine
 //node bot.js to start bot
 const Discord = require('discord.js')
+const fs = require('fs')
 const { token } = require('./constants/token')
 const client = new Discord.Client()
 
@@ -12,30 +14,48 @@ client.on('ready', () => {
     console.info(`Logged in as ${client.user.tag}!`);
 });
 
-//take command
-client.on('message', async msg => {
-    //does not work if not in a discord
-    if(!message.guild) return;
+//function to store audio files
+function out(voiceChannel, user){
+    const location = './audio/${Date.now()}.pcm';
+    return fs.createWriteStream(location);
+}
 
+//take command
+client.on('message', msg => {
     //this is the only current command !join to join your voice channel
     if(msg.content === '!join'){
-        if(message.member.voiceChannel){
-            message.member.voiceChannel.join()
+        if(msg.member.voiceChannel){
+            msg.member.voiceChannel.join()
             .then(connection => {
-                message.reply('I have joined the channel!');
+                msg.reply('I have joined the channel!');
 
                 const receiver = connection.createReceiver();
-                connection.on('speaking', (user,speaking) => {
-                    if(speaking){
-                        const audioStream = reciever.createPCMStream(user);
-                        
 
+                connection.on('speaking', (user, speaking) => {
+                    if (speaking) {
+                        //debug code
+                        msg.channel.sendMessage(`I'm listening to ${user}`);
+
+                        //this takes a pcm stream and writes it locally
+                        const input = receiver.createPCMStream(user);
+                        const output = out(voiceChannel, user);
+                        input.pipe(output);
+                        output.on("data", console.log);
+
+                        //debug code
+                        audioStream.on('end', () => {
+                            msg.channel.sendMessage(`I'm no longer listening to ${user}`);
+                        });
                     }
-                })
+                });
             })
             .catch(console.log);
         }else{
-            message.reply('Join a voice channel first!');
+            msg.reply('Join a voice channel first!');
         }
+    }
+    if(msg.content === '!leave'){
+        msg.member.voiceChannel.leave()
+        msg.reply('I have left the channel!');
     }
 });
