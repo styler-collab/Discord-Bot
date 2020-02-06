29a2c969175a2c8975d6e922c9f1f3821324b8aa
styler-collab <59663629+styler-collab@users.noreply.github.com>
2020-02-06 16:17:45 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,10 +1,9 @@
-//functionally this code just does !join and !leave voice chats and stores everything said locally
-//nodemon + ffmpeg + node-opus add to machine
+
 //node bot.js to start bot
 const Discord = require('discord.js')
-const fs = require('fs')
 const { token } = require('./constants/token')
 const client = new Discord.Client()
+const fetch = require("node-fetch");
 
 //change token in token.json
 client.login(token);
@@ -14,48 +13,22 @@ client.on('ready', () => {
     console.info(`Logged in as ${client.user.tag}!`);
 });
 
-//function to store audio files
-function out(voiceChannel, user){
-    const location = './audio/${Date.now()}.pcm';
-    return fs.createWriteStream(location);
-}
 
 //take command
 client.on('message', msg => {
     //this is the only current command !join to join your voice channel
-    if(msg.content === '!join'){
-        if(msg.member.voiceChannel){
-            msg.member.voiceChannel.join()
-            .then(connection => {
-                msg.reply('I have joined the channel!');
-
-                const receiver = connection.createReceiver();
-
-                connection.on('speaking', (user, speaking) => {
-                    if (speaking) {
-                        //debug code
-                        msg.channel.sendMessage(`I'm listening to ${user}`);
-
-                        //this takes a pcm stream and writes it locally
-                        const input = receiver.createPCMStream(user);
-                        const output = out(voiceChannel, user);
-                        input.pipe(output);
-                        output.on("data", console.log);
-
-                        //debug code
-                        audioStream.on('end', () => {
-                            msg.channel.sendMessage(`I'm no longer listening to ${user}`);
-                        });
-                    }
-                });
+    if(msg.content === '!fact'){
+        var id = Math.floor((Math.random() * 1399) + 1);
+        fetch('http://www.randomfactgenerator.net/?id=' + id).then(function(url){
+            fact = url.text().then(function(text){
+                var index = 2077
+                while (text.charAt(index) != '<'){
+                    index++;
+                }
+                var fact = text.substring(2077, index);
+                msg.channel.send(fact);
             })
-            .catch(console.log);
-        }else{
-            msg.reply('Join a voice channel first!');
-        }
-    }
-    if(msg.content === '!leave'){
-        msg.member.voiceChannel.leave()
-        msg.reply('I have left the channel!');
+        });
+
     }
 });
